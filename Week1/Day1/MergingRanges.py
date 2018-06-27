518acffea51b1c1bc50041d0a949b90b98712515
kiranmayee123 <31198714+kiranmayee123@users.noreply.github.com>
2018-06-27 09:34:31 +0530

    Add files via upload

---
+++ Week1/Day1/MergingRanges.py
@@ -0,0 +1,73 @@
+import unittest
+
+
+def merge_ranges(meetings):
+    # Merge meeting ranges
+    Merge=[];
+
+    for i in meetings:
+        start = i[0]
+        end = i[1]
+
+        for y in range(start, end):
+            if y not in Merge:
+                Merge.append(y);
+    Merge.sort()
+    result=[];
+    z=Merge[0];
+
+
+    for i in range(len(Merge)-1):
+        if ((Merge[i]+1) ==Merge[i+1]):
+            continue
+        else:
+            result.append((z,Merge[i]+1))
+            z=Merge[i+1]
+
+    result.append((z, Merge[i+1] + 1))
+
+
+    return result
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (2, 4)])
+        expected = [(1, 4)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_touch(self):
+        actual = merge_ranges([(5, 6), (6, 8)])
+        expected = [(5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meeting_contains_other_meeting(self):
+        actual = merge_ranges([(1, 8), (2, 5)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_stay_separate(self):
+        actual = merge_ranges([(1, 3), (4, 8)])
+        expected = [(1, 3), (4, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_multiple_merged_meetings(self):
+        actual = merge_ranges([(1, 4), (2, 5), (5, 8)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_not_sorted(self):
+        actual = merge_ranges([(5, 8), (1, 4), (6, 8)])
+        expected = [(1, 4), (5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_sample_input(self):
+        actual = merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+        expected = [(0, 1), (3, 8), (9, 12)]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
---
+++ Week1/Day1/Productof3numbers.py
@@ -0,0 +1,57 @@
+import unittest
+
+def highest_product_of_3(list_of_ints):
+
+    # Calculate the highest product of three numbers
+    assert (len(list_of_ints) > 2), "Exception"
+
+    list_of_ints.sort()
+    arr_size=len(list_of_ints)
+    # maxvalue=list_of_ints[0]*list_of_ints[1]*list_of_ints[2]
+    maxvalue=max(list_of_ints[arr_size-3]*list_of_ints[arr_size-2]*list_of_ints[arr_size-1],list_of_ints[0]*list_of_ints[1]*list_of_ints[arr_size-1])
+    # print(maxvalue)
+
+    return maxvalue
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_list(self):
+        actual = highest_product_of_3([1, 2, 3, 4])
+        expected = 24
+        self.assertEqual(actual, expected)
+
+    def test_longer_list(self):
+        actual = highest_product_of_3([6, 1, 3, 5, 7, 8, 2])
+        expected = 336
+        self.assertEqual(actual, expected)
+
+    def test_list_has_one_negative(self):
+        actual = highest_product_of_3([-5, 4, 8, 2, 3])
+        expected = 96
+        self.assertEqual(actual, expected)
+
+    def test_list_has_two_negatives(self):
+        actual = highest_product_of_3([-10, 1, 3, 2, -10])
+        expected = 300
+        self.assertEqual(actual, expected)
+
+    def test_list_is_all_negatives(self):
+        actual = highest_product_of_3([-5, -1, -3, -2])
+        expected = -6
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_list(self):
+        with self.assertRaises(Exception):
+            highest_product_of_3([])
+
+    def test_error_with_one_number(self):
+        with self.assertRaises(Exception):
+            highest_product_of_3([1])
+
+    def test_error_with_two_numbers(self):
+        with self.assertRaises(Exception):
+            highest_product_of_3([1, 1])
+
+
+unittest.main(verbosity=2)
